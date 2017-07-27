8e637dc66bda6315a6b2bf861ab3ae8d5fbcaa12
Cronwel Noel Irias <noelirias@gmail.com>
2017-07-27 00:16:00 -0400

    new routes complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,6 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all.order("created_at DESC")
-
   end
 
   def new
@@ -16,9 +15,31 @@ class PostsController < ApplicationController
       render "new"
     end
   end
+
   def show
     @post = Post.find(params[:id])
   end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(params[:post].permit(:title, :body))
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to root_path
+  end
+
   private
 
   def post_params
